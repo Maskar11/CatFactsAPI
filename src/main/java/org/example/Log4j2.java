115d0c8591367c947071198a6c4f894ec699a41d
Maskar11 <askarmustafa116@gmail.com>
2024-02-01 12:58:08 +0100

    Log4j2.java  modified

--- src/main/java/org/example/Log4j2.java
+++ src/main/java/org/example/Log4j2.java
@@ -3,9 +3,9 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class App {
+public class Log4j2 {
 
-    private static final Logger logger = LogManager.getLogger(App.class);
+    private static final Logger logger = LogManager.getLogger(Log4j2.class);
 
      public static void main(String[] args ) {
         System.out.println( "Hello, user!" );
