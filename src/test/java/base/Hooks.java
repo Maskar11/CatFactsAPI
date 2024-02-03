0842e05be97309886aae64bc6b124640c89c9d92
Maskar11 <askarmustafa116@gmail.com>
2024-02-03 17:55:11 +0100

    Hooks.java  modified

--- src/test/java/base/Hooks.java
+++ src/test/java/base/Hooks.java
@@ -1,6 +1,7 @@
 package base;
 
 import io.restassured.RestAssured;
+import org.example.Log4j2;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import utils.Driver;
@@ -13,11 +14,13 @@ public class Hooks extends BaseClass {
     public void setUp() {
         RestAssured.baseURI = ConfigurationReader.get("url");
         token = Token.getToken();
+        Log4j2.startLog(this.getClass().getSimpleName());
     }
 
 
     @AfterClass
     public void tearDown() {
+        Log4j2.endLog(this.getClass().getSimpleName());
         Driver.closeDriver();
     }
 
