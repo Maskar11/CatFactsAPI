7bd27c1ac55e7895e3667b079f94a6dd6bb7b835
Maskar11 <askarmustafa116@gmail.com>
2024-02-07 18:32:33 +0100

    BaseClass.java modified

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -1,16 +1,12 @@
 package base;
 
-import org.apache.logging.log4j.Logger;
 import io.restassured.response.Response;
 import org.example.Log4j2;
-import org.testng.annotations.Listeners;
-
 
 
 public class BaseClass extends Log4j2 {
 
     static public String token;
     static public Response response;
-
 
 }
