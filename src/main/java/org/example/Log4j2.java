ce2e9e5b515ea501b3c0b529162354a2c827756d
Maskar11 <askarmustafa116@gmail.com>
2024-03-29 06:03:03 +0100

    Log4j2.java modified

--- src/main/java/org/example/Log4j2.java
+++ src/main/java/org/example/Log4j2.java
@@ -9,7 +9,6 @@ public class Log4j2 {
     private static final Logger logger = LogManager.getLogger(Log4j2.class);
 
     public static void main(String[] args) {
-
         System.out.println("Hello, user!");
         logger.trace("We've just greeted the user!");
         logger.debug("We've just greeted the user!");
@@ -23,45 +22,37 @@ public class Log4j2 {
         logger.info(testClassName + " at the class level is Starting...");
     }
 
-
     protected static void startLoggerMethod(String testClassName) {
         logger.info(testClassName + " before the Test is Starting...");
     }
-
 
     public static void endLoggerMethod(String testClassName) {
         logger.info(testClassName + " after the Test is Ending...");
     }
 
-
     public static void endLoggerClass(String testClassName) {
         logger.info(testClassName + " at the class level is Ending...");
     }
-
 
     //Info Level
     public static void info(String message) {
         logger.info(message);
     }
 
-
     //Warn Level
     public static void warn(String message) {
         logger.warn(message);
     }
-
 
     //Error Level
     public static void error(String message) {
         logger.error(message);
     }
 
-
     //Fatal Level
     public static void fatal(String message) {
         logger.fatal(message);
     }
-
 
     //Debug Level
     public static void debug(String message) {
